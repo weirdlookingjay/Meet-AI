a36186b38f7ff8d7940f7b21e1e6fbb88cb58570
Joe Clark <weirdlookingjay@live.com>
2025-06-11 16:42:22 -0400

    05: authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,17 @@
+import { auth } from "@/lib/auth";
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  //turn into a boolean
+  if (!!session) {
+    redirect("/");
+  }
   return <SignInView />;
 };
 
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,18 @@
+import { auth } from "@/lib/auth";
 import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  //turn into a boolean
+  if (!!session) {
+    redirect("/");
+  }
+
   return <SignUpView />;
 };
 
